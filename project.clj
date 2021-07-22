dddf4a2bf1f58f3dc695b8ca516bac783b6339a5
Phil Hofmann <phil@200ok.ch>
2021-07-22 11:57:49 +0200

    chore: Bump to version 1.1.1-SNAPSHOT [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fsdb "1.1.0"
+(defproject fsdb "1.1.1-SNAPSHOT"
   :description "A reasonably convenient database on top of the file system."
   :url "http://gitlab.com/200ok/fsdb"
   :license {:name "Eclipse Public License"
